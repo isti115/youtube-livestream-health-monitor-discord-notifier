8522cdebdeb28de8baa8755a9ed2aae58d775611
Donkó István <istvan.donko@gmail.com>
2022-02-27 20:20:49 +0100

    Add extra check for noData to increase hysteresis

--- main.py
+++ main.py
@@ -23,20 +23,29 @@ def update(comment, st):
 def loop():
     # st = status.get()
     st = None
+    no_data_first = True
     while True:
-        print(f'check at: {datetime.datetime.now().isoformat()}')
-        new_st = status.get(settings['stream_key_name'])
         now = datetime.datetime.now()
+        print(f'check at: {now.isoformat()}')
+        new_st = status.get(settings['stream_key_name'])
         if new_st != st:
             print('diff')
-            st = new_st
-            update('Health change notice', st)
+            if new_st['healthStatus']['status'] == 'noData' and no_data_first:
+                print('noData -> retry set')
+                no_data_first = False
+                time.sleep(60 * 1)
+            else:
+                st = new_st
+                no_data_first = True
+                update('Health change notice', st)
+                time.sleep(60 * 5)
         elif now.hour == 12 and now.minute < 5:
             print('time')
             update('Midday status report', st)
+            time.sleep(60 * 5)
         else:
             print('match')
-        time.sleep(60 * 5)
+            time.sleep(60 * 5)
 
 if __name__ == "__main__":
     loop()
